3473420d1083ff113fedeea2a41e177137d42df3
Noah Heinrich <noahheinrich@gmail.com>
2016-02-03 16:12:26 -0600

    Application prints titles of three youtube videos

--- ruby/youtube.rb
+++ ruby/youtube.rb
@@ -1,15 +1,15 @@
 require 'rubygems'
 require "google/apis/youtube_v3"
-
-require "trollop"
 DEVELOPER_KEY = "AIzaSyAumOWDyFt8gBqaKztQoEPWoSQ-pMSRC5E"
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
-
+puts "Please enter what you want to search for"
+search_term = gets.chomp!
 Youtube = Google::Apis::YoutubeV3
 youtube = Youtube::YouTubeService.new
-videos = youtube.list_searches(:snippet, q: "cats")
+youtube.key = DEVELOPER_KEY
+videos = youtube.list_searches(:snippet, max_results: 3, q: search_term)
 
-videos.each do |video|
-  puts video
+videos.items.each do |video|
+  p video.to_h[:snippet][:title]
 end
