514b85d00a1fb6f7ef40a30bcd8250bba4117aa0
Noah Heinrich <noahheinrich@gmail.com>
2016-02-05 14:44:42 -0600

    Added view and render list without pagination

--- javascript/simple_backbone/quoteBackbone.js
+++ javascript/simple_backbone/quoteBackbone.js
@@ -17,14 +17,54 @@ $(function(){
 
     state: {
       firstPage: 1,
-      currentPage: 1
+      currentPage: 1,
+      pageSize: 15
     },
 
+    getResults: function () {
+      var self = this;
+      this.fetch({
+        reset: true,
+        success: function (collection, response, options) {
+            self.trigger('successOnFetch');
+          },
+          error: function (collection, response, options) {
+            self.trigger('errorOnFetch');
+          }
+      });
+    }
+
   });
 
-  var quotes = new QuoteList();
-  quotes.fetch().done(function() {
-    console.log(quotes.models);
-  });
+  var QuoteView = Backbone.View.extend({
+    el:"ul",
+    template: _.template("<%_.forEach(quotes, function (quote) {%>"
+                        + "<li>'<%= quote.attributes.quote %>'</li>"
+                        + "<p>-<%= quote.attributes.source %>, <%= quote.attributes.context %><p>"
+                        + "<% }) %>"),
+    initialize: function () {
+      this.listenTo(this.collection, 'successOnFetch', this.handleSuccess);
+      this.listenTo(this.collection, 'errorOnFetch', this.handleError);
+    },
 
+    render: function () {
+      var page = this.collection.getFirstPage();
+      console.log(page)
+      this.$el.html(this.template({ quotes: page.models }));
+      return this;
+    },
+    handleSuccess: function () {
+      this.render();
+    },
+
+    handleError: function () {
+      alert("Something went wrong")
+    },
+
+
+  })
+
+  var quotes = new QuoteList([], { mode: "client" });
+  var quoteView = new QuoteView({ collection: quotes });
+  quotes.getResults();
 });
