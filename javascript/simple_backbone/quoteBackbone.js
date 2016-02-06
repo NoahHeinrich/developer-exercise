a845b7fabd8f09ea8d0227ad190924d1ea505e66
Noah Heinrich <noahheinrich@gmail.com>
2016-02-06 10:32:07 -0600

    Finish pagination, unstyled

--- javascript/simple_backbone/quoteBackbone.js
+++ javascript/simple_backbone/quoteBackbone.js
@@ -1,5 +1,7 @@
 $(function(){
 
+  var event_bus = _({}).extend(Backbone.Events);
+
   var Quote = Backbone.Model.extend({
     defaults: function () {
       return {
@@ -32,7 +34,8 @@ $(function(){
             self.trigger('errorOnFetch');
           }
       });
-    }
+    },
+
 
   });
 
@@ -42,55 +45,107 @@ $(function(){
                         + "<li>'<%= quote.attributes.quote %>'</li>"
                         + "<p>-<%= quote.attributes.source %>, <%= quote.attributes.context %><p>"
                         + "<% }) %>"),
+
     initialize: function () {
       this.listenTo(this.collection, 'successOnFetch', this.handleSuccess);
       this.listenTo(this.collection, 'errorOnFetch', this.handleError);
+      this.listenTo(event_bus, 'renderFirstPage', this.render);
+      this.listenTo(event_bus, 'renderNextPage', this.render);
+      this.listenTo(event_bus, 'renderLastPage', this.render);
+      this.listenTo(event_bus, 'renderFinalPage', this.render);
+      this.listenTo(event_bus, 'renderPage', this.render);
     },
 
-    render: function () {
-      var page = this.collection.getFirstPage();
+    render: function (collection) {
+      var page = collection;
       this.$el.html(this.template({ quotes: page.models }));
       return this;
     },
     handleSuccess: function () {
-      console.log(this.collection)
-      this.render();
+      this.render(this.collection);
     },
 
     handleError: function () {
       alert("Something went wrong")
-    },
+    }
 
 
   })
 
   var PaginatorView = Backbone.View.extend({
-    el: ".pages",
+    // el: ".pages",
     template: _.template("<a id='first' href=#> << </a> "
                         + " <a id='back' href=#> < </a>"
+                        + " <% for (i=1; i <= this.totalPages; i++) { %>"
+                        + " <a class='numPage' href='#'> <%= i %> </a> "
+                        + "<% } %>"
                         + " <a id='forward' href=#> > </a>"
                         + " <a id= 'last' href=#> >> </a>"),
+
+    events: {
+      "click #first"  :  "goToFirst",
+      "click #back"  :  "goBack",
+      "click #forward"  :  "goForward",
+      "click #last"  :  "goToLast",
+      "click .numPage" : "goToPage"
+    },
+
+
     initialize: function () {
       this.listenTo(this.collection, 'successOnFetch', this.handleSuccess);
       this.listenTo(this.collection, 'errorOnFetch', this.handleError);
     },
 
     render: function () {
+
       this.$el.html(this.template());
       return this;
     },
 
     handleSuccess: function () {
+      this.totalPages = Math.ceil(this.collection.fullCollection.length / 15)
       this.render();
     },
 
     handleError: function () {
       alert("Something went wrong")
     },
+
+    goToFirst: function (e) {
+      e.preventDefault();
+      event_bus.trigger("renderFirstPage", this.collection.getFirstPage())
+    },
+
+    goBack: function (e) {
+      e.preventDefault();
+      if(this.collection.hasPreviousPage()){
+        event_bus.trigger("renderLastPage", this.collection.getPreviousPage())
+      }
+    },
+
+    goForward: function (e) {
+      e.preventDefault();
+      if(this.collection.hasNextPage()){
+        event_bus.trigger("renderNextPage", this.collection.getNextPage());
+      }
+    },
+
+    goToLast: function (e) {
+      e.preventDefault();
+      event_bus.trigger("renderFinalPage", this.collection.getLastPage())
+    },
+
+    goToPage: function (e) {
+      e.preventDefault();
+      var pageNum = parseInt($(e.currentTarget).text())
+
+      event_bus.trigger("renderPage", this.collection.getPage(pageNum))
+    }
   })
 
   var quotes = new QuoteList([], { mode: "client" });
-  var quoteView = new QuoteView({ collection: quotes });
-  var pagesView = new PaginatorView({ collection: quotes });
+  var quoteView = new QuoteView({ collection: quotes, el:$("ul") });
+  var pagesView = new PaginatorView({ collection: quotes, el:$(".pages") });
   quotes.getResults();
 });
+
